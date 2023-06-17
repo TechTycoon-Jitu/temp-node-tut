da07c39e8358bdcf032a93aab4b2694d594b5361
Sanket <sanketmondal893@gmail.com>
2023-06-17 11:24:41 +0530

    first commit

---
+++ 8-os-module.js
@@ -0,0 +1,16 @@
+const { log } = require('console')
+const os = require('os')
+//info about current user
+const user = os.userInfo()
+log(user)
+
+//method returns the system uptime in seconds
+console.log(`The System Uptime is ${os.uptime()} seconds`)
+
+const currentOS = {
+    name:os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+}
+console.log(currentOS)
